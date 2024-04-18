78714186780374b25c07e9bee00e1052f2fdcf62
Mochytk <116850132+Mochytk@users.noreply.github.com>
2024-04-18 15:24:30 -0400

    Añadir archivos

---
+++ Codigo Base Tarea 2-20240409/FactoryOfNumbers.c
@@ -0,0 +1,33 @@
+#include "Io.h"
+#include "Manufactura.h"
+#include "Robot.h"
+#include "Mundo.h"
+
+void construir()
+{
+
+}
+
+void destruir()
+{
+
+}
+
+void informacion()
+{
+
+}
+
+void salir()
+{
+
+}
+
+int main()
+{
+    /* Un hint
+    Los punteros void no saben a que estan apuntando,
+    me pregunto si habrá una forma de decirles
+    */
+    return 0;
+}
